9d3324d3e5103c0c24d6e7fabcdd35188d9339f2
vivek <viveklakhani1232@gmail.com>
2025-05-17 18:18:44 +0530

    add new error and add postman_curl in file

--- controllers/controller.js
+++ controllers/controller.js
@@ -47,7 +47,7 @@ group by
 res.status(200).json({dropDwonForSchool : getDropDwon.rows})
         
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json({error : "Something went wrong" })
     }
 }
 
@@ -66,8 +66,11 @@ exports.addInstitute = async (req , res ) =>{
       exma_id
         } = req.body ;
 
-        
-     const insertData = `INSERT INTO "Institute_registration".institue_registrations
+ if (!ragistration_name || !institute_id || !board_id) {
+  return res.status(400).json({ error: "Required fields are missing" });
+}
+
+ const insertData = `INSERT INTO "Institute_registration".institue_registrations
 ( ragistration_name, institute_id, board_id, medium, class_category_id, standers, subject,colleges_id,degree_id,exma_id)
 VALUES($1, $2, $3, $4, $5, $6, $7,$8, $9, $10);`;
 
@@ -88,7 +91,7 @@ VALUES($1, $2, $3, $4, $5, $6, $7,$8, $9, $10);`;
 
     } catch (error) {
         console.log('error :>> ', error);
-        res.json(500).json({error : "Institute data not rgistation"})
+        res.json(500).json({error : "Something went wrong. Please try again later."})
     }
 }
 
